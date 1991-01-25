16872c1658801bff4fd66b821f4aed219e5155d7
Peter Teuben <teuben@astro.umd.edu>
1991-01-25 20:17:46 +0000

    Initial revision

---
+++ cat/stations.pl
@@ -0,0 +1,29 @@
+data STATIONS.list
+lines 3 30
+xcolumn 1
+ycolumn 2
+limits
+ptype 3 0
+points
+box
+xlabel East-West (nanosecs)
+ylabel North (nanosecs)
+relocate -500 500
+label HAT CREEK ANTENNA STATIONS
+lines 32 32
+xcol 1
+ycol 2
+expand 8
+ptype 4 0
+points
+lines 34 34
+xcol 1
+ycol 2
+EXPAND 1
+relocate 120 260
+label LAB
+lines 34 34
+ptype 3 0
+xcolumn 1
+ycolumn 2
+points
