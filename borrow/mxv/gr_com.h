09224e199d883245b2b355747950b6dac53e8fb7
Peter Teuben <teuben@astro.umd.edu>
1992-06-25 19:35:41 +0000

    UIUC 15-feb-2001 w/out wip and some manuals

---
+++ borrow/mxv/gr_com.h
@@ -0,0 +1,10 @@
+/*
+ *	File:		gr_com.h
+ *	Contents:	Includes all the necessary headers for the
+ *				graphics module
+ */
+
+#include "gr_inc.h"
+#include "gr_def.h"
+#include "gr_ext.h"
+#include "gr_var.h"
