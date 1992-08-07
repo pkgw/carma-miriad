81005adb541dfd1d22146fd938fdaa296989ecff
Peter Teuben <teuben@astro.umd.edu>
1992-08-07 02:45:26 +0000

    Initial revision

---
+++ borrow/mxv/mxv.for
@@ -0,0 +1,12 @@
+c= mxv - MIRIAD X-window Visualizer
+c& hr
+c: visual display
+c+
+c   MXV is an X-windows based package for displaying 2D and 3D images.
+c   It reads MIRIAD, FITS, and HDF data formats.  MXV should be invoked 
+c   separately, in background, before being used.  Remember that the
+c   DISPLAY environment variable must be set to your local machine
+c   before being used.
+c
+c   Additional documentation will appear in the future.
+c--
