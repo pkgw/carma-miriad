5151c74d2041e0e150444b0521146e25a1536d4e
Peter Teuben <teuben@astro.umd.edu>
1992-08-26 14:42:15 +0000

    UIUC 15-feb-2001 w/out wip and some manuals

---
+++ borrow/mxv/td_var.h
@@ -0,0 +1,12 @@
+/*
+ *	File:		td_var.h
+ *	Contents:	Header file containing all global variables used
+ *				by the td module.
+ */
+
+static	char	*td_dirDefName=".";
+char		*td_logFile;  /* will be init to $HOME/mxvLog */
+#if 0
+char		*td_dirName;
+time_t	lastDirMod;
+#endif
