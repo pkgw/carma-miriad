e2b4cdd0551660d5d7dd8964d2c8678cb18ca535
Peter Teuben <teuben@astro.umd.edu>
1992-09-28 19:48:06 +0000

    Initial revision

---
+++ borrow/mxv/version.h
@@ -0,0 +1,12 @@
+/* version.h
+
+Whatever version we are calling this today.
+*/
+/* Major version */
+#define	MAJOR	2
+#define	MINOR	1
+#define EDIT 980
+/* Either "E" or " " (blank) */
+#ifndef STATUS
+#define	STATUS	" "
+#endif
