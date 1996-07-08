89505ee69a2e097fc4dc616ed75b581d0c207e41
Peter Teuben <teuben@astro.umd.edu>
1996-07-08 18:55:24 +0000

    Initial revision

---
+++ src/prog/vis/uvgen.h
@@ -0,0 +1,23 @@
+c--------------------------------------------------
+c	uvgen.h - include file for uvgen program
+c--------------------------------------------------
+c
+c  This include file is used to return the correlator setups
+c  from the various "coram" routines to the main routine.
+c
+c  Outputs:
+c    LO description:			lo1,lo2,freqif
+c    Wide correlator description:	nwide,wfreq,wwidth
+c    Spectral correlator description:	numchan,nspect,sdf,sfreq,ischan,nschan
+c
+	integer maxspect
+	parameter(maxspect=8)
+        integer maxsrc,maxpol,maxpnt,maxpolar
+        parameter(maxsrc=1000,maxpol=4,maxpnt=100,maxpolar=20)
+c
+	double precision sfreq(maxspect),sdf(maxspect),lo1,lo2,freqif
+	real wfreq(maxspect),wwidth(maxspect)
+	integer numchan,nwide,nspect,ischan(maxspect),nschan(maxspect)
+	common/uvgencom/sfreq,sdf,lo1,lo2,freqif,wfreq,wwidth,
+     *	  numchan,nwide,nspect,ischan,nschan
+
