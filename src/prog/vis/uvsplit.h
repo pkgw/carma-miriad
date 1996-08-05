5bd47a180b0cead2e44e9800bf06c07e5de0302a
Peter Teuben <teuben@astro.umd.edu>
1996-08-05 07:20:28 +0000

    Initial revision

---
+++ src/prog/vis/uvsplit.h
@@ -0,0 +1,12 @@
+	include 'maxdim.h'
+	integer MAXFILES,MAXOPEN
+	parameter(MAXFILES=500,MAXOPEN=6)
+c
+	integer npol,nopen,nfiles,ifno(MAXFILES),indx(MAXFILES),nwins
+	integer vCheck(MAXFILES),vCopy(MAXFILES),lOut(MAXFILES),lVis
+	logical done(MAXFILES),doif,dowide,docomp,wins(MAXWIN)
+	character out(MAXFILES)*64,version*64
+c
+	common/Files/npol,nopen,nfiles,ifno,indx,vCheck,vCopy,
+     *	    lOut,lVis,nwins,done,doif,dowide,docomp,wins
+	common/FilesC/out,version
