2fcc8a428c23145e36dec523150b8b27ec0db019
Peter Teuben <teuben@astro.umd.edu>
2002-08-07 03:06:36 +0000

    doc

--- src/prog/analysis/polargrid.for
+++ src/prog/analysis/polargrid.for
@@ -30,6 +30,7 @@ c     Optionally these three numbers can be followed by the
 c     coordinate type (a string). Currently supported are 'linear' and
 c     'logarithmic'. The units of the radii are in the units of that
 c     coordinate system.
+c     Example: -1,2,0.01 is a logarithmic axis from 0.1" to 100"
 c     By default the all pixels will fit in the output map, with a
 c     linear grid and stepsize of 1 pixel.
 c@ angle
@@ -50,6 +51,7 @@ c     dark ages Gruendl/Vogel - developed under zodiac
 c     13-feb-02 pjt  Miriadized this routine
 c     16-feb-02 pjt  first public version
 c     24-jul-02 pjt  0 maskvalue, not -1
+c      6-aug-02 pjt  doc
 c***********************************************************************
 c
       include   'maxdim.h'
@@ -57,7 +59,7 @@ c
       integer   MAXNAX
       parameter (MAXNAX=3)
       character VERSION*(*)
-      parameter (VERSION='24-jul-2002')
+      parameter (VERSION='6-aug-2002')
 c
       character infile*128, oufile*128, rmode*10, ctype1*10,ctype2*10
       integer   irow(MAXDIM),ioutrw(MAXDIM)
