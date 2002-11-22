eab63a1ddeb79ef3d8dffef9450891b797e62253
Melvyn Wright <wright@astro.berkeley.edu>
2002-11-22 18:06:41 +0000

    increased maxdim, maxant.

--- src/inc/maxdim.h
+++ src/inc/maxdim.h
@@ -1,18 +1,38 @@
 c=======================================================================
 c - MAXBUF tells us how many words of memory we can use for data
+c  History:
+c    mchw 06mar91  Changed maxbuf on ral
+c    mjs  29jun92  maxdim of 2048 at UIUC/astro
+c    mjs  18feb93  maxchan of 2048 at UIUC/astro
+c    mjs  27feb93  maxdim of 32768 at UIUC/astro
+c    mchw 10apr94  Added MAXWIN=16
+c    mchw 01aug96  Added MAXWIDE=18
+c    rjs  02apr98  Set MAXBASE=MAXANT*(MAXANT+1)/2
+c    mchw 01feb02  decouple MAXBASE from MAXANT
+c    mchw 04feb02  change PARAMETER(MAXBUF=1048576) to PARAMETER(MAXBUF=4194304)
+c    mchw 14feb02  recouple MAXBASE to MAXANT as we need selfcal on MAXANT.
+c
 	INTEGER   MAXBUF
-        PARAMETER(MAXBUF=4194304)
+#ifdef unicos
+	PARAMETER(MAXBUF=2097152)
+#endif
+#ifdef convex
+	PARAMETER(MAXBUF=4194304)
+#endif
+#ifdef alliant
+	PARAMETER(MAXBUF=4194304)
+#endif
+#ifndef MAXBUF
+	PARAMETER(MAXBUF=4194304)
+#endif
 c-----------------------------------------------------------------------
 c - MAXDIM is an often used parameter, to indicate maximum size of maps
 	INTEGER   MAXDIM
-	PARAMETER(MAXDIM=2048)
-c - MAXDIM1 should be used rarely, for big maps that are never handled in 2D
-	INTEGER   MAXDIM1
-	PARAMETER(MAXDIM1=16384)
+	PARAMETER(MAXDIM=8192)
 c-----------------------------------------------------------------------
-c		maximum number of antennae (HC=12, WSRT=14, VLA=28)
+c		maximum number of antennas (HC=3/6/9/..., WSRT=14, VLA=27)
 	INTEGER   MAXANT
-	PARAMETER(MAXANT=30)
+	PARAMETER(MAXANT=500)
 
 c		maximum number of baselines
 	INTEGER   MAXBASE
@@ -20,13 +40,14 @@ c		maximum number of baselines
 
 c		maximum number of channels in spectral data
 	INTEGER   MAXCHAN
-	PARAMETER(MAXCHAN=2048)
+	PARAMETER(MAXCHAN=4096)
 
-c		maximum number of windows in visibility data
+c		maximum number of windows in spectral data
 	INTEGER   MAXWIN
 	PARAMETER(MAXWIN=16)
-c
-c		maximum number of wideband channels.
-	INTEGER MAXWIDE
+
+c		maximum number of wideband channels
+	INTEGER   MAXWIDE
 	PARAMETER(MAXWIDE=18)
 c=======================================================================
+
