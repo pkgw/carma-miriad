04bf13c1d08b5b917990e9c24e17985a292b0cb3
Peter Teuben <teuben@astro.umd.edu>
2005-01-03 14:36:46 +0000

    (atnf) merged in ovro/12m changes

--- src/subs/pb.for
+++ src/subs/pb.for
@@ -58,8 +58,8 @@ c   26mar97   rjs    Less precision in pbencode.
 c   07jul97   rjs    Change call to coaxdesc to coaxget.
 c   05sep97   mchw   Change lower freq for HATCREEK to 24 GHz.
 c   09may00   rjs    Add extra check.
-c   23jun03   pjt    add LOFAR
-c   30jun04   gmx    Updated WSRT beam
+c   10may02   rjs    Add model for OVRO.
+c   13oct03   rjs    Added Ravi's 12mm model.
 c************************************************************************
 c* pbList -- List known primary beam types.
 c& rjs
@@ -641,9 +641,11 @@ c------------------------------------------------------------------------
 c
 c Set coefficients for each telescope and frequency range
 c
-	integer NCOEFF,NATCAL1,NATCAL2,NATCAL3
+	integer NCOEFF,NATCAL1,NATCAL2,NATCAL3,NATCAK
 	parameter(NCOEFF=5,NATCAL3=3,NATCAL1=5,NATCAL2=7)
+	parameter(NATCAK=4)
 	real atcas(NCOEFF),atcac(NCOEFF),atcax(NCOEFF)
+	real atcak(NATCAK)
 	real atcal3(NATCAL3),atcal1(NATCAL1),atcal2(NATCAL2)
 	real vla(NCOEFF)
 c
@@ -659,6 +661,11 @@ c
 	data atcas /1.0, 1.02e-3, 9.48e-7, -3.68e-10, 4.88e-13/
 	data atcac /1.0, 1.08e-3, 1.31e-6, -1.17e-9,  1.07e-12/
 	data atcax /1.0, 1.04e-3, 8.36e-7, -4.68e-10, 5.50e-13/
+c
+c Model by Ravi at 22.235 GHz - 4th order poly.
+c
+	data atcak /1.0, -9.5793797E-04, 3.2279621E-07, 
+     *			 -3.8065801E-11/
 c
 	data vla /0.9920378, 0.9956885e-3, 0.3814573e-5, -0.5311695e-8,
      *		  0.3980963e-11/
@@ -696,31 +703,36 @@ c
      *			NCOEFF,atcac,'Reciprocal 4th order poly')
 	call pbAdd('ATCA',    7.90,9.3,     50.6, 0.03,  IPOLY,
      *			NCOEFF,atcax,'Reciprocal 4th order poly')
+	call pbAdd('ATCA',    15.5,25.5,    50.6, 0.10,  POLY,
+     *			NATCAK,atcak,'Fourth order poly')
+	call pbAdd('ATCA.2',  15.5,25.5,    50.6, 0.03,  IPOLY,
+     *			NCOEFF,atcax,'Reciprocal 4th order poly')
 c
 c  VLA primary beam is taken from AIPS code.
 c
 	call pbAdd('VLA',     0.071,24.510, 44.3, 0.023,IPOLY,
      *			NCOEFF,vla,'Reciprocal 4th order poly')
-
+c
+c  The OVRO primary beam is a gaussian of size is 128.4 arcmin.GHz
+c  according to numbers from Shardha Jogee.
+c
+	call pbAdd('OVRO',24.0,350.0,   107.3, 0.05, GAUS,0,0.,
+     *				   'Truncated Gaussian')
 c
 c  The Hat Ck primary beam is a gaussian of size is 191.67 arcmin.GHz
 c  according to "John L"
 c
-	call pbAdd('HATCREEK',24.0,270.0,   191.67, 0.05, GAUS,0,0.,
+	call pbAdd('HATCREEK',24.0,350.0,   191.67, 0.05, GAUS,0,0.,
      *				   'Truncated Gaussian')
 c
 c  The following values for the WSRT are derived from the NEWSTAR
 c  manual, which gives pb = cos**6(beta*freq(MHz)*angle(degrees))
 c  where beta = 0.0629 for f < 500 MHz, and 0.065 for f > 500 MHz.
 c  These numbers look a bit large (WSRT under-illuminated?).
-c  GMX (30Jun2004): added a factor 1.07 to the second line. This
-c  number came out of new measurements of the primary beam characteristics, 
-c  but is (as yet) not written in stone. Check with Rob Braun or 
-c  Tom Oosterloo for more info.
 c
 	call pbAdd('WSRT',    0.0,0.5,	     51.54, 0.02,  COS6,0,0.,
      *				   'Cos**6 function')
-	call pbAdd('WSRT',    0.5,8.0,	49.87*1.07, 0.02,  COS6,0,0.,
+	call pbAdd('WSRT',    0.5,8.0,	     49.87, 0.02,  COS6,0,0.,
      *				   'Cos**6 function')
 c
 c  Miscellaneous.
@@ -731,12 +743,6 @@ c
      *				   'Truncated Gaussian')
 	call pbAdd('SINGLE',  0.0,999.,	      0.00, 0.5,  SINGLE,0,0.,
      *				   'Single dish')
-c
-c  LOFAR - for simulations
-c
-	call pbAdd('LOFAR',    0.071,24.510, 44.3, 0.023,IPOLY,
-     *			NCOEFF,vla,'Reciprocal 4th order poly')
-
 c
 	end
 c************************************************************************
