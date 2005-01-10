c37a10edef7a794868a2a5b91234c717b86f64cd
Peter Teuben <teuben@astro.umd.edu>
2005-01-10 02:20:30 +0000

    added a simple uvio test

--- install/testmirlib.c
+++ install/testmirlib.c
@@ -1,5 +1,12 @@
 /*
  *  test out a bunch of things the C version of mirlib does
+ *
+ *  gcc -g -Imirlib -o testmirlib testmirlib.c mirlib/libmir.a -lm
+ * or:
+ *  gcc -g -I$MIRINC -I$MIRSUBS -o testmirlib testmirlib.c -L$MIRLIB -lmir -lm
+ *  [except it seems to need pgplot now, and it should not !!! ]
+ *  gcc -g -I$MIRINC -I$MIRSUBS -o testmirlib testmirlib.c $MIRLIB/libmir.a -lm
+ *  [prevents the  pgplot problem]
  */
 
 
@@ -13,11 +20,13 @@
 
 #define check(iostat) if(iostat)bugno_c('f',iostat)
 
-static char *name1 = "test1.mir";
 
-void test_hio()
+void test_hio(char *name1)
 {
   int t1, i1,iostat;
+  double pi = 3.141592;
+  float g = 9.8;
+  int d = 28;
   
 
   hopen_c(&t1, name1, "old", &iostat);
@@ -28,8 +37,14 @@ void test_hio()
     fprintf(stderr,"Creating new dataset %s\n",name1);
 
   hopen_c(&t1, name1, "new", &iostat);                check(iostat);
-  haccess_c(t1,&i1,"a","new",&iostat);                check(iostat);
+
+  wrhdd_c(t1,"pi",pi);
+  wrhdr_c(t1,"g",g);
+  wrhdi_c(t1,"d",d);
+
+  haccess_c(t1,&i1,"a","write",&iostat);              check(iostat);
   hdaccess_c(i1,&iostat);                             check(iostat);
+
 #if 0
   hdelete_c(t1,"a",&iostat);                          check(iostat);
 #endif
@@ -37,11 +52,43 @@ void test_hio()
 
 }
 
+void test_uvio(char *fname, int nc, int nw, int nr)
+{
+  int t1, i,iostat;
+  double preamble[5];
+  float data[2*MAXCHAN];
+  int  flags[MAXCHAN];
+
+  /* delete old one , if exists */
+  hopen_c(&t1, fname, "old", &iostat);
+  if (iostat==0) hrm_c(t1);
+
+  uvopen_c(&t1, fname, "new");
+
+  for (i=0; i<nr; i++) {
+
+    uvputvr_c(t1,H_BYTE,"abyte",(char *)data, 1);
+    uvputvr_c(t1,H_INT2,"aint2",(char *)data, 1);
+    uvputvr_c(t1,H_INT, "aint4",(char *)data, 1);
+    uvputvr_c(t1,H_REAL,"areal",(char *)data, 1);
+    uvputvr_c(t1,H_DBLE,"adble",(char *)data, 1);
+    uvputvr_c(t1,H_CMPLX,"acmplx",(char *)data, 1);
+    
+    uvwrite_c(t1,preamble,data,flags,nc);
+    uvwwrite_c(t1,data,flags,nw);
+  }
+  uvclose_c(t1);
+}
+
 int main(int argc, char *argv[])
 {
   fprintf(stderr,"Testing MIRLIB\n");
 
-  test_hio();
+  test_hio("test1.mir");
+
+  test_uvio("test1.uv", 1024, 16, 4);
+ 
+  return 0;
 }
 
 
