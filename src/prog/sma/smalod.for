315eede251b9d2f6910fb1c2f7ec6a427a91b470
Jun-Hui Zhao <jzhao@cfa.harvard.edu>
2006-06-13 17:48:11 +0000

    update the version date

--- src/prog/sma/smalod.for
+++ src/prog/sma/smalod.for
@@ -292,11 +292,12 @@ c                  allowing presence of empty data chunks.
 c    jhz 09-jun-06 fixed a bug in sma_mirRead.c in parsing source if
 c                  in the case no source information is given in
 c                  mir data (an on-line bug)
+c    jhz 12-jun-06 updated version date
 c------------------------------------------------------------------------
         integer maxfiles
         parameter(maxfiles=128)
         character version*(*)
-        parameter(version='SmaLod: version 1.27 09-jun-06')
+        parameter(version='SmaLod: version 1.27 12-jun-06')
 c
         character in(maxfiles)*64,out*64,line*64, rxc*4
         integer tno, length, len1
