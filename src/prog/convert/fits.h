0022db3b64021f377040a375926cd59d6f78ed36
Jun-Hui Zhao <jzhao@cfa.harvard.edu>
2007-01-12 17:58:37 +0000

    This submission is for Bob Sault.

--- src/prog/convert/fits.h
+++ src/prog/convert/fits.h
@@ -1,7 +1,7 @@
 	include 'maxdim.h'
 c
-	integer EQUATOR,ALTAZ
-	parameter(EQUATOR=1,ALTAZ=0)
+	integer EQUATOR,ALTAZ,NASMYTH
+	parameter(EQUATOR=1,ALTAZ=0,NASMYTH=4)
 c
 	integer uvCrval,uvCdelt,uvCrpix
 	integer uvStokes,uvFreq,uvRa,uvDec
