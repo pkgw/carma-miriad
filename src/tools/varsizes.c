2981a7b681c218b686768723b60b960cf010757a
Peter Teuben <teuben@astro.umd.edu>
2007-03-12 20:53:20 +0000

    added size_t and off_t

--- src/tools/varsizes.c
+++ src/tools/varsizes.c
@@ -1,13 +1,18 @@
 /*============================================================================
 *  Simple program to return variable sizes used on your platform.
+*
 *  To build: cc -o varsizes varsizes.c
 *
+*  On some 32 bit architecture machine in order to get off_t to be 8 byte, add
+#      -D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE -D_LARGEFILE64_SOURCE
 *  Author: Chris.Phillips@csiro.au
 *  Date: 2005-03-31
 *  $Id$
 *===========================================================================*/
 
 #include<stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 int main (void)
 {
@@ -24,6 +29,9 @@ int main (void)
   printf(" Sizeof double = %d\n", sizeof(double));
   printf(" Sizeof long double = %d\n", sizeof(long double));
   printf("\n");
+  printf(" Sizeof size_t = %d\n", sizeof(size_t));
+  printf(" Sizeof off_t = %d\n",  sizeof(off_t));
+
 
   return 0;
 }
