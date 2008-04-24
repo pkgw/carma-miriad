70ee33b26fa78e238bf775fd31ff805d4c4b408f
Peter Teuben <teuben@astro.umd.edu>
2008-04-24 17:28:27 +0000

    bug out of auto corr data used, they index wrong for now

--- src/subs/uvgetbl.for
+++ src/subs/uvgetbl.for
@@ -11,6 +11,10 @@ c
 	complex data(nread)
 c
 c  Determine the baseline number, and conjugate the data if necessary.
+c  The baseline number is a number between 1 and N*(N-1)/2 for
+c  pure cross correlating pairs.
+c  This routine does not allow auto-correlations to occur in the data.
+c
 c
 c  Input:
 c    nread	The number of channels.
@@ -26,10 +30,12 @@ c
 c    mchw --apr93 Extracted from a task and installed as a genl subr.
 c    mjs  22apr93 Trivial doc mod.
 c    27oct00 mchw Changes to support more antennas.
+c    24apr08 pjt  Barf if auto correlations
 c------------------------------------------------------------------------
 	integer i, ant1, ant2
 c
         call basant(preambl(4),ant1,ant2)
+	if (ant1.eq.ant2)call bug('f','uvgetbl: cannot handle autocorr')
 	if(ant1.le.ant2)then
 	  bl = ((ant2-2)*(ant2-1))/2 + ant1
 	else
