4a8228e1d917517227de042d009610889e2a3d40
Peter Teuben <teuben@astro.umd.edu>
2008-07-14 22:05:26 +0000

    print out a few more things

--- install/miralloc.for
+++ install/miralloc.for
@@ -1,12 +1,17 @@
       PROGRAM miralloc
 c
 c   allocate memory, miriad style
+c 
+c   for sizes < MAXBUF you will see it will use a pointer < MAXBUF
+c   and each invocation will return the same number
+c   for sizes > MAXBUF memory is obtained from the OS stack,
+c   and each invocation will return a different number
 c
 c
       include 'maxdim.h'
       include 'mem.h'
 
-      PTRDIFF_T pData
+      PTRDIFF_T pData,nx1,ny1,nz1,ntot
       INTEGER nx,ny,nz
 
       CALL keyini
@@ -14,10 +19,19 @@ c
       CALL keyi('ny',ny,4)
       CALL keyi('nz',nz,4)
       CALL keyfin
+      nx1 = nx
+      ny1 = ny
+      nz1 = nz
+      ntot = nx1*ny1*nz1
+
+	write(*,*) 'nx*ny*nz=',nx*ny*nz
+	write(*,*) 'ntot    =',ntot
+	write(*,*) 'MAXBUF  =',MAXBUF
+
 
       CALL MemAlloc(pData, nx*ny*nz, 'r')
 
-	write(*,*) 'pData=',pData
+	write(*,*) 'pData   =',pData
 
       CALL myWork(memr(pData),nx,ny,nz)
 
