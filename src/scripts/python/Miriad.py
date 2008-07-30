0d3025df7357f106ba8a103accc7004d375eae3c
Peter Teuben <teuben@astro.umd.edu>
2008-07-30 16:35:36 +0000

    (experimental) new version with enhanced help option for values

--- src/scripts/python/Miriad.py
+++ src/scripts/python/Miriad.py
@@ -5,10 +5,10 @@
 ## a debug level.  Finally, includes the warning, error, and dprintf functions
 ## which may also be useful in scripting.
 ##
-##   15-mar-2003   Created                                   PJT\n
-##   16-apr-2003   added run,keyr,keyi,keya\n
-##   05-mar-2004   Added help comments nemo style            NLC\n
-##   16-may-2004   Deleted all the code we don't use for map2  NLC\n
+##   15-mar-2003   Created                                   PJT
+##   16-apr-2003   added run,keyr,keyi,keya
+##   05-mar-2004   Added help comments nemo style            NLC
+##   16-may-2004   Deleted all the code we don't use for map2  NLC
 ##   19-feb-2008   Changed keya, keyi, and keyr to not use the 
 ##                 keypresent function.  This allows the programmer to
 ##                 specify a default keyword value of nothing.  However,
@@ -18,27 +18,104 @@
 ##                 to remove extraneous comments, combined the badkeyword
 ##                 function with keya, and added the keyf function which works
 ##                 the same as keyr.
+##   28-jul-2008   merged map2's miriad.py in miriad'd Miriad.py      PJT
+##   30-jul-2008   moved back old useful functions for Miriad         PJT
 ##
-import sys, os
+##  Example usage:
+##    help = """
+##           bla bla
+##           """
+##    keyval = {
+##       "key1" :  "val1\n  help1",
+##       "key2" :  "val2\n  help2"
+##       }
+##
+##    import Miriad
+##    Miriad.keyini(keyval,usage=help)
+##    a = Miriad.keya('key1')
+
+##
+import sys, os, string
 
 #   some global variables (private to Miriad.py)
-_version = "1.5 (19-feb-2008)"
-_quit    = 0
-_mkeyval = {}
-_help    = {}
+_version    = "2.0 (28-jul-2008)"
+_quit       = 0
+_mkeyval    = {}
+_help       = {}
 _debuglevel = 0
+_logger     = ""
+
+def run(command,log,fatal):
+    """an alias for miriad()"""
+    return miriad(command,log,fatal)
+
+def miriad(command,log=0,fatal=1):
+    """
+    miriad        execute a miriad 'command' (as a list of strings) and accumulate a log
+                  either from the default logfile (see setlogger) or by overriding using
+                  log=. By default an error results into an exit, though this can be
+                  overriden by using fatal=0
+                  Example:    miriad(['itemize','in=ngc1365.cm'],log='ngc1365.log')
+
+    Other commands available in PYRAMID are:
+    
+    doc           show specific help for a MIRIAD task , e.g. doc('invert')
+    keys          show all keywords for a MIRIAD task , e.g. keys('invert')
+    setlogger     change the default logger for subsequent miriad() commands,
+                  e.g. setlogger('rubbish.log')
+
+    zap           delete a miriad dataset (with existence check), e.g. zap('ngc1365.cm')
+    zap_all       delate a set of data (unchecked), e.g. zap_all('ncg1365*')
+    grepcmd       Return the Nth word on the first occurence of a string match from a command
+                  e.g.:    grepcmd('histo in=ngc1365.cm','Maximum',2)
+    greplog       Return the Nth word on the first occurence of a string match from a logfile
+                  e.g.:    grepcmd('ngc1365.log','naxis1',2)
+                  Notice that the index N starts at 0!!
+
+    Some useful classes defined via PYRAMID:
+
+    Timer         mark and compute CPU times between sections of the code
+
+    A number of useful and less useful user contribued PYRAMID scripts
+    can be found in and below $MIR/examples/
+    
+    """
+    global _logger
+    if (log != 0):
+        mycmd = cmd(command) + '> %s 2>&1' % log
+    else:
+        mycmd = cmd(command) + _logger;
+    # debug
+    print "MIRIAD% ",mycmd
+    retval = os.system(mycmd)
+    if retval:
+        print "###: Error %d from %s" % (retval,command[0])
+        if fatal:
+            os._exit(retval)
+
+def setlogger(log,append=0):
+    """ set the logfile for the miriad() function. By default any old logfile is removed"""
+    global _logger
+    print "setlogger: default logfile now " + log
+    _logger = '>> %s 2>&1' % log
+    if append==0:
+        zap(log)
+
+
 
 def keyini(keyval,show=0,gui=0,usage=None):
     """Initialize the values of all keywords, and check to see that
        all required keywords are present.  Also, it user specifies
        help, it calls the show_keyval function
 
-       @param keyval A dictionary of keywords and values
+       @param keyval A dictionary of keywords and values (special: _msg_)
+       @param help   A helpful usage reminder what the program does
        @param show   If 1, calls show_keyval
        @param gui    If 1, ignores required keywords (???)
 
        @return None"""
     global _quit, _mkeyval, _help, _debuglevel
+
     _help = keyval.copy()
     for key in _help.keys():  # Parse for help comments if any
        idx = _help[key].find('\n')
@@ -46,6 +123,7 @@ def keyini(keyval,show=0,gui=0,usage=None):
           _help[key] = _help[key][idx+1:].strip()
           keyval[key] = keyval[key][:idx].strip()
        elif key == '_msg_':
+          # deprecated, should use the originally intended help
           pass # do nothing since this is the usage statement
        else: # No help comment found
           _help[key] = ""
@@ -187,9 +265,9 @@ def keyb(key):
    
       Can understand True,False,1,0,yes, and no as all valid."""
    temp = keya(key)
-   if temp.lower() in ('1','yes','true','y'):
+   if temp.lower() in ('1','yes','true','y', 't'):
       return True
-   elif temp.lower() in ('0','no','false','n'):
+   elif temp.lower() in ('0','no','false','n', 'f'):
       return False
    else:
       error("%s is not a boolean value!" %temp)
@@ -212,6 +290,11 @@ def show_keyval(keyval,help=0,quit=0):
     if quit:
         os._exit(0)
 
+def version():
+    # print "Miriad.py: version %s" % _version
+    return _version;
+
+
 def warning(text):
    """Print a string of text as a warning"""
    sys.stderr.write("### Warning! %s\n" %text)
@@ -230,6 +313,113 @@ def dprint(level,text):
       error('%s is not a valid integer' %str(level))
    if x <= _debuglevel:
       print text
+
+def zap(file):
+    """ remove a possibly existing miriad dataset (no wildcards)"""
+    if os.path.isdir(file):
+        os.system('rm -rf ' + file)
+    elif os.path.isfile(file):
+        os.system('rm -f ' + file)
+
+def zap_all(files):
+    """ remove all datasets (wildcards now allowed via the shell)"""
+    os.system('rm -rf ' + files)
+
+def cmd(cmdlist,debug=0):
+    """convert a list of strings to a command string"""
+    str = cmdlist[0] 
+    for arg in cmdlist[1:]:
+        str = str + ' ' +  arg
+    if debug: print str
+    return str
+
+def doc(task):
+    """show a little help on a miriad task...."""
+    os.system('doc %s | less' % task)
+
+def keys(task):
+    """show just the keywords for a miriad task"""
+    os.system('doc %s | grep ^Keyword' % task)
+
+def timer(slot):
+    """ save CPU info in predefined global slots"""
+    global timet, timec
+    timec[slot] = time.clock()
+    timet[slot] = time.time()
+
+def cpulen(a,b):
+    """create a string to print human readable CPU info between slots """
+    st = '%.3f ' % (timet[b]-timet[a])
+    sc = '%.3f ' % (timec[b]-timec[a])
+    return sc + st
+
+#   should use popen()
+def grepcmd(cmd,word,index=0):
+    """run a command, grep through the output for a word and return a 0-indexed word"""
+    log = 'tmp.log'
+    os.system(cmd + ' > %s' % log)
+    f = open(log,"r")
+    v = f.read()
+    va=string.split(v,"\n")
+    f.close()
+    for s in va:
+        if string.find(s,word)>=0:
+            sa=string.split(s)
+            # print 'Match ' + word + '=>' + sa[index]
+            return sa[index]
+    print 'No match on' + word
+    return "no-match"
+
+def greplog(log,word,index=0):
+    """ grep through a logfile for a word and return a 0-indexed word"""
+    f = open(log,"r")
+    v = f.read()
+    va=string.split(v,"\n")
+    f.close()
+    for s in va:
+        if string.find(s,word)>=0:
+            sa=string.split(s)
+            # print 'Match ' + word + '=>' + sa[index]
+            return sa[index]
+    print 'No match on' + word
+    return "no-match"
+
+class Timer:
+    """a class to help you compute CPU times the script takes
+    between certain tagged locations, e.g.
+              t=Timer()
+              t.tag()
+              .....
+              t.tag()
+              print "Time passed %g seconds" % t.dt(0,1)
+    """
+    
+    def __init__(self):
+        self.n = 0
+        self.timec = []
+        self.timet = []
+    def count(self):
+        return self.n
+    def tag(self):
+        self.n = self.n + 1
+        self.timec.append(time.clock())
+        self.timet.append(time.time())
+    def show(self):
+        print "Currently have %d entries" % self.n
+        print time.clock()
+        print time.time()
+    def showall(self):
+        print self.timec
+        print self.timet
+    def clock(self,i):
+        return self.timec[i]
+    def time(self,i):
+        return self.timet[i]
+    def dt(self,i,j):
+        return self.timet[j]-self.timet[i]
+    def dc(self,i,j):
+        return self.timec[j]-self.timec[i]
+    
 
 #   If executed... probably they are in an interactive python shell
 if __name__ == '__main__':
