78cc402823d59aa7b65e5db72b1e696ada9544b1
Peter Teuben <teuben@astro.umd.edu>
2009-07-05 18:30:44 +0000

    added ruvlist, and sanitized rgplist

--- src/scripts/python/pacs.py
+++ src/scripts/python/pacs.py
@@ -24,6 +24,11 @@ buddy_a09 = [ ]
 def sexa2dec(dms):
     """
     convert an D:M:S string to D, also works for H:M:S
+
+    Inputs:
+        dms     D:M:S string, e.g. "04:05:22.3"
+    Returns:
+        converted floating point value of D:M:S
     """
     w = dms.split(':')
     if len(w) == 3:
@@ -60,7 +65,7 @@ def nint(x):
     if x>0: return int(x+0.5)
     return int(x-0.5)
 
-def rglist(file):
+def rgplist(file):
     """
     This reads the ascii output of a listing from gplist options=phase
     which looks roughly as follows:
@@ -72,6 +77,11 @@ def rglist(file):
         05:32:21.0    0   96    0 -152   53  -13  101 -132   33    0  -30 -138   97  -90    9
         05:56:29.5    0  121    0 -147   66  -15   83 -135   18    0  -55 -146   88  -98   18
         ...
+
+    Inputs:
+      file       ...
+    Output:
+      (n,t,r)    ....
     """
     fp = open(file)
     lines = fp.readlines()
@@ -84,7 +94,7 @@ def rglist(file):
     for l in lines:
         w = l.split()
         if inHeader:
-            if w[0] == 'Time': 
+            if len(w) > 0 and w[0] == 'Time': 
                 inHeader = False
         else:
             if nants == 0:
@@ -97,7 +107,100 @@ def rglist(file):
             r.append( avector(w[1:]) )
     return (nants,np.array(t), np.array(r).transpose())
             
- 
+
+def ruvlist(file):
+    """
+    This reads the ascii output of a listing from uvlist recnum=0
+    which looks roughly as follows:
+
+UVLIST: version  10-dec-08
+ ***** UV Listing for vis1 *****
+  Options: brief,data
+  No. channels:   1, Linetype: channel
+  Line Start:    1.000, Width:   90.000, Step:   90.000
+  Scale factor:    1.000
+
+------------------------------------------------------------
+
+Data values for 09JAN17
+
+ Vis #    Time      Ant    Pol U(kLam)  V(kLam)    Amp   Phase
+     1 09:43:52.5   1-   2 RR   -38.69   258.92    4.071 -146
+     2 09:43:52.5   1-   3 RR  -308.83   187.31    1.926 -132
+     3 09:43:52.5   1-   4 RR    27.14   496.13    4.477  176
+
+        ...
+
+     The data is assumed in regular order , time and baseline regular
+
+    Inputs:
+      file       ...
+    Output:
+      (n,t,r)    ....
+    """
+    fp = open(file)
+    lines = fp.readlines()
+    fp.close()
+    inHeader = True
+
+    t=[]
+    r=[]
+    nrec = 0
+    time = ""
+    ntime = 0
+    nbl = 0
+    a1 = []
+    a2 = []
+    vec = []
+    nerr = 0
+    for l in lines:
+        w = l.split()
+        if inHeader:
+            if len(w) > 0 and w[0] == 'Vis': 
+                inHeader = False
+        else:
+            nrec = nrec + 1
+            if w[1] != time:
+                ntime = ntime + 1
+                if ntime == 2:
+                    nbl = nrec - 1
+                time = w[1]
+                # print 'new Time: %d %s' % (ntime,time)
+                if ntime > 1:
+                    if len(vec) == nbl:  
+                        t.append( sexa2dec(time) )
+                        r.append( avector(vec) )
+                    else:
+                        nerr = nerr + 1
+                    vec = []
+            ant1 = w[2][:-1]
+            ant2 = w[3]
+            if ntime == 1:
+                a1.append(int(ant1))
+                a2.append(int(ant2))
+            if w[8][-1] == '*':
+                vec.append(0.0)
+            else:
+                vec.append(float(w[8]))
+    # and the final one
+    if len(vec) == nbl:
+        t.append( sexa2dec(time) )
+        r.append( avector(vec) )
+    else:
+        nerr = nerr + 1
+    if nerr > 0:
+        print "Not a regular visibility dataset, skipping %d/%d timeslots" % (nerr,ntime)
+
+    if False:
+        print "%d records" % nrec
+        print "%d timeslots" % ntime
+        print "%d baselines" % nbl
+        print "ant1: ",a1
+        print "ant2: ",a2
+
+    return (nbl,np.array(t),np.array(r).transpose())
+            
+
 def pplot(t,p,s='ro-'):
     """add an unwrapped phase plot"""
     plt.plot(t,unwrap(p),s)
@@ -158,7 +261,7 @@ def figure():
     plt.show()
 
 def example1(file):
-    (nants,t,r) = rglist(file)
+    (nants,t,r) = rgplist(file)
     plt.plot(t,r[4],'ro-')
     plt.plot(t,r[5],'bs-')
     plt.axis([0,24,-180,180])
@@ -168,7 +271,7 @@ def example1(file):
 
 
 def example2(file):
-    (nants,t,r) = rglist(file)
+    (nants,t,r) = rgplist(file)
     plt.figure(1)
     plt.subplot(221)
     plt.plot(t,r[0],'ro-')
@@ -193,16 +296,16 @@ def example2(file):
     plt.show()
 
 def example3(file,ant,doit=True):
-    (nants,t,r) = rglist(file)
+    (nants,t,r) = rgplist(file)
     plt.plot(t,r[ant-1],'ro-')
     plt.show()
 
-def example4(file1,file2):
+def example4(file1,file2,pair=1):
     """feed it a CARMA and SZA dataset"""
-    (n1,t1,p1) = rglist(file1)
-    (n2,t2,p2) = rglist(file2)
-    fit4(1,t1,p1,t2,p2)
-    
+    (n1,t1,p1) = rgplist(file1)
+    (n2,t2,p2) = rgplist(file2)
+    fit4(pair,t1,p1,t2,p2)
+
 
 
 if __name__ == '__main__':
