6b29f558ef82719bde293380f4ddf6dc25d6608e
Peter Teuben <teuben@astro.umd.edu>
2009-09-08 21:44:48 +0000

    via ATNF version, a gfortran optimization

--- src/subs/powell.for
+++ src/subs/powell.for
@@ -4,7 +4,7 @@ c& bpw
 c: optimization,mathematics
 c+
       subroutine powell(x,e,n,f,escale,icon,mxiter,calcfx,w,maxfun,
-     *								ifail)
+     *                  ifail)
 c
       implicit none
       integer n,icon,mxiter,maxfun,ifail
@@ -30,35 +30,38 @@ c  minimum is found.
 c
 c  2) Arguments:
 c
-c    n		must be set to the number of variables.
+c    n          must be set to the number of variables.
 c
-c    x and e 	These are one dimensional arrays. on entry to the routine
-c		x(i) must be set to an approximation to the ith variable
-c		and e(i) to the accuracy to which its optimum value is
-c		required. On exit x(i) will be set to the calculated optimum
-c		value of the ith variable. It is assumed that the magnitudes
-c		of the parameters e(i) are approximately proportional to
-c		the magnitudes of the corresponding variables x(i).
+c    x and e    These are one dimensional arrays.  On entry to the
+c               routine x(i) must be set to an approximation to the ith
+c               variable and e(i) to the accuracy to which its optimum
+c               value is required.  On exit x(i) will be set to the
+c               calculated optimum value of the ith variable.  It is
+c               assumed that the magnitudes of the parameters e(i) are
+c               approximately proportional to the magnitudes of the
+c               corresponding variables x(i).
 c
-c    f		Thus will be set to the minimum value of the function.
+c    f          Thus will be set to the minimum value of the function.
 c
-c    escale	This limits the maximum change in the variables at a single
-c		step.  x(i) will not be changed by more than escale*e(i).
+c    escale     This limits the maximum change in the variables at a
+c               single step.  x(i) will not be changed by more than
+c               escale*e(i).
 c
-c    icon	This must be set to 1 or 2. It controls the ultimate
-c		convergence criterion (see section 5).
+c    icon       This must be set to 1 or 2. It controls the ultimate
+c               convergence criterion (see section 5).
 c
-c    maxit	The routine will be left regarless after maxit iterations 
-c    maxfun	have been completed, or maxfun function evaluations.
+c    maxit      The routine will be left regarless after maxit
+c    maxfun     iterations have been completed, or maxfun function
+c               evaluations.
 c
-c    w		Scratch array.
+c    w          Scratch array.
 c
-c    ifail	Error return.
-c		Ifail = 0:   Success
-c			1    Maximum change does not alter function
-c			2    Accuracy limited by errors in calcfx.
-c			3    Max iterations performed within Powell
-c			4    Max function values evaluated in Powell
+c    ifail      Error return.
+c               Ifail = 0:   Success
+c                       1    Maximum change does not alter function
+c                       2    Accuracy limited by errors in calcfx.
+c                       3    Max iterations performed within Powell
+c                       4    Max function values evaluated in Powell
 c
 c  3) Method.
 c  This will be published by m.j.d. powell in the july or october
@@ -110,42 +113,48 @@ c--
 c  History:
 c    rjs Dark-ages Stolen from somewhere.
 c    rjs  25jan90  Minor documentation improvement.
+c
+c $Id$
 c----------------------------------------------------------------------
       integer nfcc,i,j,jj,jjj,k,ind,inn,iterc,isgrad,itone,ixp,idirn
       integer iline,is,jil
       real scer,ddmag,fkeep,fp,sum,dmax,ddmax,dmag,dacc,dl,d,da,dd,fa
       real fprev,fb,db,fc,dc,fi,di,a,b,fhold,aaa
-c
+
       ifail = 0
-      ddmag=0.1*escale
-      scer=0.05/escale
-      jj=n*n+n
-      jjj=jj+n
-      k=n+1
-      nfcc=1
-      ind=1
-      inn=1
-      do 1 i=1,n
-      do 2 j=1,n
-      w(k)=0.
-      if(i-j)4,3,4
-3     w(k)=abs(e(i))
-      w(i)=escale
-4     k=k+1
+      ddmag = 0.1*escale
+      scer  = 0.05/escale
+      jj  = n*n + n
+      jjj = jj + n
+      k = n + 1
+      nfcc = 1
+      ind = 1
+      inn = 1
+      do 2 i = 1, n
+        do 1 j = 1, n
+          w(k) = 0.0
+          if (i.eq.j) then
+            w(k) = abs(e(i))
+            w(i) = escale
+          end if
+          k = k + 1
+1       continue
 2     continue
-1     continue
+
       iterc=1
       isgrad=2
       call calcfx(n,x,f)
       fkeep=abs(f)+abs(f)
-5     itone=1
+
+5     itone = 1
       fp=f
       sum=0.
       ixp=jj
       do 6 i=1,n
-      ixp=ixp+1
-      w(ixp)=x(i)
+        ixp=ixp+1
+        w(ixp)=x(i)
 6     continue
+
       idirn=n+1
       iline=1
 7     dmax=w(iline)
@@ -153,8 +162,8 @@ c
       dmag=amin1 (ddmag,0.1*dmax)
       dmag=amax1(dmag,20.*dacc)
       ddmax=10.*dmag
-      go to (70,70,71),itone
-70    dl=0.
+      if (itone.eq.3) go to 71
+      dl=0.
       d=dmag
       fprev=f
       is=5
@@ -164,58 +173,61 @@ c
       dl=d
 58    k=idirn
       do 9 i=1,n
-      x(i)=x(i)+dd*w(k)
-      k=k+1
+        x(i)=x(i)+dd*w(k)
+        k=k+1
 9     continue
       call calcfx(n,x,f)
       nfcc=nfcc+1
-      if(nfcc-maxfun) 22,22,8003
-   22 continue
+      if (nfcc.gt.maxfun) go to 999
       go to (10,11,12,13,14,96),is
-14    if(f-fa)15,16,24
-16    if(abs(d)-dmax) 17,17,18
-17    d=d+d
-      go to 8
-c
-c  Maximum change does not alter function.
-c
-18    ifail = 1
-      return
-15    fb=f
-      db=d
-      go to 21
-24    fb=fa
-      db=da
-      fa=f
-      da=d
-21    go to (83,23),isgrad
+
+
+14    if (f.lt.fa) then
+        fb = f
+        db = d
+      else if (f.eq.fa) then
+        if (abs(d).gt.dmax) then
+          ifail = 1
+          return
+        end if
+        d = d + d
+        go to 8
+      else
+c       Maximum change does not alter function.
+        fb = fa
+        db = da
+        fa = f
+        da = d
+      end if
+
+      go to (83,23),isgrad
 23    d=db+db-da
       is=1
       go to 8
 83    d=0.5*(da+db-(fa-fb)/(da-db))
       is=4
-      if((da-d)*(d-db)) 25,8,8
+      if ((da-d)*(d-db).ge.0.0) go to 8
 25    is=1
-      if(abs(d-db)-ddmax) 8,8,26
+      if (abs(d-db).le.ddmax) go to 8
 26    d=db+sign (ddmax,db-da)
       is=1
       ddmax=ddmax+ddmax
       ddmag=ddmag+ddmag
-      if(ddmax-dmax) 8,8,27
-27    ddmax=dmax
+      if (ddmax.le.dmax) go to 8
+      ddmax=dmax
       go to 8
-13    if(f-fa) 28,23,23
+13    if (f.ge.fa) go to 23
 28    fc=fb
       dc=db
 29    fb=f
       db=d
       go to 30
-12    if(f-fb) 28,28,31
-31    fa=f
+12    if (f.le.fb) go to 28
+      fa=f
       da=d
       go to 30
-11    if(f-fb) 32,10,10
-32    fa=fb
+11    if (f.ge.fb) go to 10
+      fa=fb
       da=db
       go to 29
 71    dl=1.
@@ -229,166 +241,182 @@ c
       dc=d
 30    a=(db-dc)*(fa-fc)
       b=(dc-da)*(fb-fc)
-      if((a+b)*(da-dc)) 33,33,34
-33    fa=fb
-      da=db
-      fb=fc
-      db=dc
-      go to 26
-34    d=0.5*(a*(db+dc)+b*(da+dc))/(a+b)
+      if ((a+b)*(da-dc).le.0.0) then
+        fa=fb
+        da=db
+        fb=fc
+        db=dc
+        go to 26
+      end if
+
+      d=0.5*(a*(db+dc)+b*(da+dc))/(a+b)
       di=db
       fi=fb
-      if(fb-fc) 44,44,43
-43    di=dc
-      fi=fc
-44    go to (86,86,85),itone
-85    itone=2
-      go to 45
-86    if(abs(d-di)-dacc) 41,41,93
-93    if(abs(d-di)-0.03*abs(d)) 41,41,45
-45    if((da-dc)*(dc-d)) 47,46,46
-46    fa=fb
-      da=db
-      fb=fc
-      db=dc
-      go to 25
-47    is=2
-      if((db-d)*(d-dc)) 48,8,8
-48    is=3
+      if (fb.gt.fc) then
+        di=dc
+        fi=fc
+      end if
+
+      if (itone.eq.3) then
+        itone = 2
+      else
+        if (abs(d-di).le.dacc) go to 41
+        if (abs(d-di).le.0.03*abs(d)) go to 41
+      end if
+
+      if ((da-dc)*(dc-d).ge.0.0) then
+        fa=fb
+        da=db
+        fb=fc
+        db=dc
+        go to 25
+      end if
+      is=2
+      if ((db-d)*(d-dc).lt.0.0) is = 3
       go to 8
+
 41    f=fi
       d=di-dl
       dd=((dc-db)*(dc-da)*(da-db)/(a+b))
       dd=sqrt(abs(dd))
       do 49 i=1,n
-      x(i)=x(i)+d*w(idirn)
-      w(idirn)=dd*w(idirn)
-      idirn=idirn+1
+        x(i)=x(i)+d*w(idirn)
+        w(idirn)=dd*w(idirn)
+        idirn=idirn+1
 49    continue
       w(iline)=w(iline)/dd
       iline=iline+1
-51    go to (55,38),itone
-55    if(fprev-f-sum) 94,95,95
-95    sum=fprev-f
-      jil=iline
-94    if (idirn-jj) 7,7,84
-84    go to (92,72),ind
+      if (itone.eq.2) go to 38
+      if (fprev.ge.f+sum) then
+        sum=fprev-f
+        jil=iline
+      end if
+      if (idirn.le.jj) go to 7
+      if (ind.eq.2) go to 53
 92    fhold=f
       is=6
       ixp=jj
       do 59 i=1,n
-      ixp=ixp+1
-      w(ixp)=x(i)-w(ixp)
+        ixp=ixp+1
+        w(ixp)=x(i)-w(ixp)
 59    continue
       dd=1.
       go to 58
-96    go to (112,87),ind
-  112 if(fp-f) 37,37,91
-91    d=2.*(fp+f-2.*fhold)/(fp-f)**2
-      if(d*(fp-fhold-sum)**2-sum) 87,37,37
-87    j=jil*n+1
-      if(j-jj) 60,60,61
-60    do 62 i=j,jj
-      k=i-n
-      w(k)=w(i)
-62    continue
-      do 97 i=jil,n
-      w(i-1)=w(i)
-97    continue
-61    idirn=idirn-n
-      itone=3
+96    if (ind.ne.2) then
+        if (fp.le.f) go to 37
+        d=2.*(fp+f-2.*fhold)/(fp-f)**2
+        if (d*(fp-fhold-sum)**2.ge.sum) go to 37
+      end if
+      j=jil*n+1
+      if (j.le.jj) then
+        do 62 i=j,jj
+          k=i-n
+          w(k)=w(i)
+62      continue
+        do 97 i=jil,n
+          w(i-1)=w(i)
+97      continue
+      end if
+      idirn=idirn-n
+      itone = 3
       k=idirn
       ixp=jj
       aaa=0.
       do 65 i=1,n
-      ixp=ixp+1
-      w(k)=w(ixp)
-      if(aaa-abs(w(k)/e(i))) 66,67,67
-66    aaa=abs(w(k)/e(i))
-67    k=k+1
+        ixp=ixp+1
+        w(k)=w(ixp)
+        if (aaa.lt.abs(w(k)/e(i))) aaa = abs(w(k)/e(i))
+        k=k+1
 65    continue
       ddmag=1.
       w(n)=escale/aaa
       iline=n
       go to 7
+
 37    ixp=jj
       aaa=0.
       f=fhold
       do 99 i=1,n
-      ixp=ixp+1
-      x(i)=x(i)-w(ixp)
-      if(aaa*abs(e(i))-abs(w(ixp))) 98,99,99
-98    aaa=abs(w(ixp)/e(i))
+        ixp=ixp+1
+        x(i)=x(i)-w(ixp)
+        if (aaa*abs(e(i)).lt.abs(w(ixp))) aaa = abs(w(ixp)/e(i))
 99    continue
-      go to 72
+      go to 53
 38    aaa=aaa*(1.+di)
-      go to (72,106),ind
-   72 continue
-53    go to (109,88),ind
-109   if(aaa-0.1) 89,89,76
-89    if(icon.eq.1)return
-116   ind=2
-      go to (100,101),inn
-100   inn=2
-      k=jjj
-      do 102 i=1,n
-      k=k+1
-      w(k)=x(i)
-      x(i)=x(i)+10.*e(i)
-102   continue
-      fkeep=f
-      call calcfx (n,x,f)
-      nfcc=nfcc+1
-      if(nfcc-maxfun) 36,36,8003
-   36 continue
-      ddmag=0.
-      go to 108
-76    if(f-fp) 35,78,78
+      if (ind.eq.2) go to 106
+53    if (ind.eq.2) go to 88
+      if (aaa.le.0.1) then
+        if (icon.eq.1)return
+        ind = 2
+        if (inn.eq.2) go to 101
+        inn = 2
+        k = jjj
+        do 102 i = 1, n
+          k = k + 1
+          w(k) = x(i)
+          x(i) = x(i) + 10.0*e(i)
+102     continue
+        fkeep = f
+        call calcfx (n,x,f)
+        nfcc = nfcc + 1
+        if (nfcc.gt.maxfun) go to 999
+        ddmag = 0.0
+        go to 108
+      end if
+
+      if (f.lt.fp) go to 35
 c
 c  Accuracy limited by errors in calcfx
 c
-78    ifail = 2
+      ifail = 2
       return
 88    ind=1
 35    ddmag=0.4*sqrt(abs(fp-f))
       isgrad=1
 108   iterc=iterc+1
-      if(iterc.le.mxiter) goto 5
+      if (iterc.le.mxiter) goto 5
 c
 c  Max iterations performed
 c
       ifail = 3
-      if(f.le.fkeep) return
-110   f=fkeep
+      if (f.le.fkeep) return
+      f=fkeep
       do 111 i=1,n
-      jjj=jjj+1
-      x(i)=w(jjj)
+        jjj=jjj+1
+        x(i)=w(jjj)
 111   continue
       return
-101   jil=1
+
+101   jil = 1
       fp=fkeep
-      if(f-fkeep) 105,78,104
-104   jil=2
-      fp=f
-      f=fkeep
-105   ixp=jj
+      if (f.eq.fkeep) then
+        ifail = 2
+        return
+      else if (f.gt.fkeep) then
+        jil = 2
+        fp = f
+        f  = fkeep
+      end if
+
+      ixp=jj
       do 113 i=1,n
-      ixp=ixp+1
-      k=ixp+n
-      go to (114,115),jil
-114   w(ixp)=w(k)
-      go to 113
-115   w(ixp)=x(i)
-      x(i)=w(k)
+        ixp=ixp+1
+        k=ixp+n
+        if (jil.eq.1) then
+          w(ixp)=w(k)
+        else
+          w(ixp)=x(i)
+          x(i)=w(k)
+        end if
 113   continue
       jil=2
       go to 92
-106   if(aaa.le.0.1) return
-107   inn=1
+106   if (aaa.le.0.1) return
+      inn=1
       go to 35
 c
 c  Max function values evaluated in powell.
 c
-8003  ifail = 4
+ 999  ifail = 4
       return
       end
