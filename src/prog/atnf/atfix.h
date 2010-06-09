9a16875d35d930a8f7e1064d65ace92d9d84b7b9
Peter Teuben <teuben@astro.umd.edu>
2010-06-09 11:44:14 +0000

    carma patch to limit memory

--- src/prog/atnf/atfix.h
+++ src/prog/atnf/atfix.h
@@ -11,7 +11,9 @@ c
 	integer MAXTCAL,TMNONE,TMCONST,TMEXTRAP,TMINTERP
 	parameter(TMNONE=1,TMCONST=2,TMEXTRAP=3,TMINTERP=4)
 c       allow for 10 days worth of syscal records, at one every 10sec.
-	parameter(MAXTCAL=10*24*60*60/10)
+c	parameter(MAXTCAL=10*24*60*60/10)
+c       for CARMA MAXANT and MAXWIN too big to allow a big MAXTCAL
+	parameter(MAXTCAL=1000)
 	double precision ttime(MAXTCAL)
 	integer tfreq(MAXTCAL),ntcal,vtcal1,vtcal2,nants,t1,t2,tmode
 	real xtrec(MAXANT,MAXWIN,MAXTCAL),ytrec(MAXANT,MAXWIN,MAXTCAL)
