ef4696a7fb7d3889141a3914989e2263b153a675
Peter Teuben <teuben@astro.umd.edu>
2012-05-01 15:49:41 +0000

    this causes mafia to allocate HUGE static arrays, which now swamps most reasonable machines. so i've put it back to something smaller, if you have the memory, comment in/out the MAXSIM and MAXFLD parameters.

--- src/prog/analysis/maxfld.h
+++ src/prog/analysis/maxfld.h
@@ -1,7 +1,11 @@
 c     gjdw 09jun11 Replaced MAXDIM by MAXSIM in mafia.for and maxfld.h
+c     pjt  may2012 Put smaller values for more generic machines,the default need big memory
       include 'maxdim.h'
       include 'maxnax.h'
       integer MAXFLD
-      parameter (MAXFLD=128*MAXDIM)
+c      parameter (MAXFLD=128*MAXDIM)
+      parameter (MAXFLD=16384)
       INTEGER   MAXSIM
-      PARAMETER(MAXSIM=8192)
+c      PARAMETER(MAXSIM=8192)
+      PARAMETER(MAXSIM=256)
+ 
