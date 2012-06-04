3b11ff8274ad350d95aebd352657d4e78823c22c
Peter Teuben <teuben@astro.umd.edu>
2012-06-04 21:15:15 +0000

    more space

--- src/prog/atnf/atfix.h
+++ src/prog/atnf/atfix.h
@@ -10,10 +10,8 @@ c
 	
 	integer MAXTCAL,TMNONE,TMCONST,TMEXTRAP,TMINTERP
 	parameter(TMNONE=1,TMCONST=2,TMEXTRAP=3,TMINTERP=4)
-c       allow for 10 days worth of syscal records, at one every 10sec.
-c	parameter(MAXTCAL=10*24*60*60/10)
-c       for CARMA MAXANT and MAXWIN too big to allow a big MAXTCAL
-	parameter(MAXTCAL=1000)
+c       allow for 5 days worth of syscal records, at one every 10sec.
+	parameter(MAXTCAL=5*24*60*60/10)
 	double precision ttime(MAXTCAL)
 	integer tfreq(MAXTCAL),ntcal,vtcal1,vtcal2,nants,t1,t2,tmode
 	real xtrec(MAXANT,MAXWIN,MAXTCAL),ytrec(MAXANT,MAXWIN,MAXTCAL)
