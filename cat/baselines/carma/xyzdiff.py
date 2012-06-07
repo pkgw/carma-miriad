8fc5f910b3d851598263f53693e0983874d43aa6
Peter Teuben <teuben@astro.umd.edu>
2012-06-07 17:52:56 +0000

    fix /bin/env and allow listobs output

--- cat/baselines/carma/xyzdiff.py
+++ cat/baselines/carma/xyzdiff.py
@@ -1,24 +1,53 @@
-#! /bin/env python
+#! /usr/bin/env python
 #
 #
 #   Script to difference two XYZ antenna positions
 #
 #   27-oct-2011      written,    Peter Teuben
+#    7-jun-2012      allow output of listobs
 
 import math,sys
 
+
+def isListobs(lines):
+    """check if file is a listobs log file"""
+    if lines[0].rfind('SUMMARY') >= 0:
+        return True
+    for i in range(3):
+        if lines[i].rfind('listobs') >= 0:
+            return True
+    return False
+    
+
 def read_antpos(file):
-    """read an antpot file, usually from $CARMA/baselines/carma"""
+    """read an antpot file, usually from $CARMA/baselines/carma
+       optionally, if it appears the output of LISTOBS,it will
+       try and parse that
+    """
     fp = open(file)
     lines = fp.readlines()
     fp.close()
     xyz=[]
+    Qlo = isListobs(lines)
+    inTable = False
     for line in lines:
         if line[0] == '#': continue
         w = line.split()
-        x = float(w[0])
-        y = float(w[1])
-        z = float(w[2])
+        if Qlo:
+            # direct output from listobs
+            if inTable and w[0] == 'Antenna':
+                x = float(w[2])
+                y = float(w[3])
+                z = float(w[4])
+            else:
+                if not inTable:
+                    inTable = w[0]=='X' and w[1]=='Y' and w[2]=='Z'
+                continue
+        else:
+            # simple X Y Z table
+            x = float(w[0])
+            y = float(w[1])
+            z = float(w[2])
         xyz.append( (x,y,z) )
     return xyz
 
@@ -61,4 +90,4 @@ if __name__ == '__main__':
 	m,s = mean_sigma(d)
 	print "Mean: %f   Sigma: %f" % (m,s)
     else:
-        print 'Need two antpos filenames'
+        print 'Need two antpos filenames (listobs output will also work)'
