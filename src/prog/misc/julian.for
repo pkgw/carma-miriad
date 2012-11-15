2bd61c0b7f4bbdd71f8e0fa5fcc12edb10e19929
Peter Teuben <teuben@astro.umd.edu>
2012-11-15 22:42:49 +0000

    options=date

--- src/prog/misc/julian.for
+++ src/prog/misc/julian.for
@@ -10,6 +10,7 @@ c
 c  History:
 c      mwp  27-aug-1999 original version
 c      pjt   2-dec-2005 added MJD
+c      pjt  12-dec-2012 cleaned up and added options=date
 c
 c------ Inline doc (retrieved with doc to a .doc file) --------------72]
 c
@@ -57,12 +58,15 @@ C       to the date formats given above [default=H]. This input is ignored for
 C 	date to Julian day conversions 
 C
 c@ options
+c       date  - do conversion of dates, no display of julian
+c
 C	quiet - report only the answer and not other info. Useful
 C		if you are using this program in a script.
 c    
 c   
 c-----------------------------------------------------------------------
 	character version*(*)
+	double precision jday0
 
 c the string date to convert
         character*32 thedate
@@ -73,20 +77,20 @@ C the julian day
 c which way are we going? forward=true means date-->julian day
 	logical forward
 c verbose output format or not
-	logical quiet
+	logical quiet,qdate
 
 C referenced routines
 	integer len1
 	logical islowerf
 C
-	parameter(version='(Version 2-dec-2005)')
-	quiet = .false.
+	parameter(version='(Version 15-nov-2012)')
+	parameter(jday0=2400000.5d0)
 c
 	call keyini
 	call keyd('jday',jday,-999d0)
 	call keya('date',thedate,' ')
 	call keya('format',theform,'H')
-	call GetOpt(quiet)
+	call GetOpt(quiet,qdate)
 	call keyfin
 
 	if(.not.quiet) call output('JULIAN: '//version)
@@ -101,71 +105,73 @@ C check inputs
 C
 	if(islowerf(theform)) call ucase(theform)
 
- 	if ( (theform.ne.'D').and.(theform.ne.'F')  
+ 	if (  (theform.ne.'D').and.(theform.ne.'F')  
      *   .and.(theform.ne.'H').and.(theform.ne.'T')
      *   .and.(theform.ne.'V')) then 
-		call bug('f','Format must be one of D,F,H,T,V')
+		call bug('f','format= must be one of D,F,H,T,V')
 	endif
 
-	if (jday.eq.-999.) then
-		forward=.true.
-		if (thedate.eq.' ') then 
-			call todayjul(jday)
-		else 
-			call dayjul(thedate,jday)
-		endif
+	if (jday.lt.0d0) then
+	   forward=.true.
+	   if (thedate.eq.' ') then 
+	      call todayjul(jday)
+	   else 
+	      call dayjul(thedate,jday)
+	   endif
+	   if (qdate) then
+	      call julday(jday,theform,thedate)
+	      forward = .false.
+	   endif
 	else 
-		if (thedate.ne.' ') then 
-		    call bug('w',
-     *               'Specify only one of date or jday. Ignoring date.')
-		endif
-		forward=.false.
-		call julday(jday,theform,thedate)
+	   if (thedate.ne.' ') then 
+	      call bug('w',
+     *           'Specify only one of date= or jday=. Ignoring date.')
+	   endif
+	   forward=.false.
+	   call julday(jday,theform,thedate)
 	endif
 
 	if(forward) then 
-		if(thedate.eq.' ') call julday(jday,theform,thedate)
-		if(.not.quiet) then
-			write(6,1001) thedate(1:len1(thedate)), jday
-			write(6,1005) thedate(1:len1(thedate)), 
-     *                                                  jday-2400000.5d0
-		else
-			write(6,1003) jday
-		endif
+	   if(thedate.eq.' ') call julday(jday,theform,thedate)
+	   if(.not.quiet) then
+	      write(6,1001) thedate(1:len1(thedate)), jday
+	      write(6,1005) thedate(1:len1(thedate)), jday-jday0
+	   else
+	      write(6,1003) jday
+	   endif
 	else 
-		if(.not.quiet) then
-			write(6,1002) jday,thedate(1:len1(thedate))
-		else
-			write(6,1004) thedate(1:len1(thedate))
-		endif
+	   if(.not.quiet) then
+	      write(6,1002) jday,thedate(1:len1(thedate))
+	   else
+	      write(6,1004) thedate(1:len1(thedate))
+	   endif
 	endif
-	if(.not.quiet) write(6,800) 
 
-  800   format('Have a nice day.')
- 1001   format(a,' is Julian Day ',f14.6)
- 1002   format('Julian Day ',f14.6,' is the date ',a)
- 1003   format(f14.6)
+ 1001   format(a,' is Julian Day ',f15.6)
+ 1002   format('Julian Day ',f15.6,' is the date ',a)
+ 1003   format(f15.6)
  1004   format(a)
  1005   format(a,' is Modified Julian Day ',f14.6)
 
 	
 	end
 c********1*********2*********3*********4*********5*********6*********7**
-        subroutine GetOpt(quiet)
+        subroutine GetOpt(quiet,date)
 c
         implicit none
-        logical quiet
+        logical quiet,date
 c
 c  Get extra processing options.
 c------------------------------------------------------------------------
         integer nopts
-        parameter(nopts=1)
+        parameter(nopts=2)
         logical present(nopts)
         character opts(nopts)*8
-        data opts/'quiet '/
+        data opts/'quiet ','date '/
 c
         call options('options',opts,present,nopts)
         quiet = present(1)
+        date  = present(2)
         end
 
 
