f3d7001c3c3da0b054ee60ee331f2d6ddeda3acc
Peter Teuben <teuben@astro.umd.edu>
2015-02-06 15:37:26 +0000

    (via ATNF) ptrdiff corrections

--- src/prog/analysis/convol.for
+++ src/prog/analysis/convol.for
@@ -18,23 +18,21 @@ c       but see the "asymmetric" option if this is not so.  If this is
 c       not given, then a Gaussian beam must be specified by the fwhm
 c       and pa parameters.
 c@ fwhm
-c       This is used to specify the gaussian beam used when convolving.
-c       Normally it gives the size of the beam to convolve with, but
-c       using options=final causes CONVOL to interpret the parameters
-c       as the required resolution of the output image.
-c       The size, in arcsec, will normally be two numbers, giving the
-c       full-width at half-maximum of the major and minor axes of the
-c       gaussian. If only one number is given, the gaussian will have
-c       equal major and minor axes.
 c       This parameter is ignored if the "beam" keyword is given.
+c       Otherwise, it specifies the width of the Gaussian convolving
+c       function unless options=final, in which case it specifies the
+c       required resolution of the output image.  The size, in arcsec,
+c       will normally be two numbers giving the full-width at half-
+c       maximum of the major and minor axes of the Gaussian.  At least
+c       one number must be given.  If only one is given, the Gaussian
+c       will have equal axes.
 c@ pa
-c       The position angle, in degrees, of the gaussian beam.
-c       Normally this is the position angle of the beam that is used
-c       when convolving. However options=final causes this parameter to
-c       be interpreted as the required position angle in the effective
-c       beam of the output image.
-c       It is measured north through towards east, in degrees.
 c       This parameter is ignored if the "beam" keyword is given.
+c       Otherwise, it specifies the position angle of the Gaussian
+c       convolving function, unless options=final, in which case it
+c       specifies the required position angle of the effective beam of
+c       the output image.  It is measured north through towards east, in
+c       degrees.  Default 0.
 c@ region
 c       The region of the input map to convolve.  See the Users Manual
 c       for instructions on how to specify this.  The default is the
@@ -73,6 +71,8 @@ c       warning and uses a scale factor of 1.
 c@ sigma
 c       When doing devonvolution (options=divide), this gives a noise
 c       parameter. Default is 0.
+c
+c$Id$
 c--
 c  History:
 c    rjs,mchw 18aug89 Converted from RESTORE.
@@ -99,38 +99,37 @@ c    rjs   05dec97 Change order of boxmask and boxinfo calls.
 c    bpw   12mar99 Increase size of map/beam/out to 512 to allow
 c                  directories.
 c    dpr   21jun01 Doc change only
+c    mhw   27oct11 Use ptrdiff type for memory allocations
 c  Bugs:
-c
-c $Id$
 c-----------------------------------------------------------------------
       include 'maxdim.h'
       include 'maxnax.h'
       include 'mirconst.h'
-      integer maxbox,maxruns
-      parameter(maxruns=3*maxdim)
-      parameter(maxbox=1024)
-      character map*512,beam*512,out*512
-      integer nsize(MAXNAX),naxis,ifail
-      integer lMap,lBeam,lOut,iref,jref,blc(3),trc(3)
-      integer xmin,xmax,ymin,ymax,nx,ny,n1,n2,xoff,yoff
-      integer nPoint,nRuns,k,l,Box(maxbox),Runs(3,maxRuns)
-      double precision cdelt1,cdelt2
-      real crpix1,crpix2,bmaj,bmin,bpa,bmaj1,bmin1,bpa1,factor,sigma
-      real temp
-      character bunit*32, flags*4, text*80, version*80
-      logical divide,selfscal,rect,asym,corr,doscale,dogaus,final
-c
-      integer handle,pDat
       include 'mem.h'
-c
-c  Externals.
-c
-      logical BoxRect,keyprsnt
-      character itoaf*8, versan*80
+
+      integer    MAXBOX, MAXRUNS
+      parameter (MAXBOX  = 1024, MAXRUNS = 3*maxdim)
+
+      logical   asym, corr, divide, dogaus, doscale, final, rect,
+     *          selfscal
+      integer   Box(MAXBOX), Runs(3,MAXRUNS), blc(3), ifail,
+     *          iref, jref, k, l, lBeam, lMap, lOut, n1, n2, nPoint,
+     *          nRuns, naxis, nsize(MAXNAX), nx, ny, trc(3), xmax,
+     *          xmin, xoff, ymax, ymin, yoff
+      ptrdiff   handle, pDat
+      real      bmaj, bmaj1, bmin, bmin1, bpa, bpa1, crpix1, crpix2,
+     *          factor, sigma, temp
+      double precision cdelt1, cdelt2
+      character beam*512, bunit*32, flags*4, map*512, out*512, text*80,
+     *          version*72
+
+      logical   BoxRect,keyprsnt
+      character itoaf*8, versan*72
+      external  boxrect, itoaf, keyprsnt, versan
 c-----------------------------------------------------------------------
       version = versan('convol',
-     :                 '$Revision$',
-     :                 '$Date$')
+     *                 '$Revision$',
+     *                 '$Date$')
 c
 c  Get the input parameters.
 c
@@ -138,35 +137,35 @@ c
       call keya('map',Map,' ')
       call keya('beam',Beam,' ')
       doGaus = Beam.eq.' '
-      if(doGaus)then
-        call keyr('fwhm',bmaj1,0.)
+      if (doGaus) then
+        call keyr('fwhm',bmaj1,0.0)
         call keyr('fwhm',bmin1,bmaj1)
-        call keyr('pa',bpa1,0.)
+        call keyr('pa',bpa1,0.0)
       endif
       call keya('out',Out,' ')
-      call BoxInput('region',map,box,maxbox)
+      call BoxInput('region',map,box,MAXBOX)
       call GetOpt(final,divide,asym,corr)
-      selfscal = .not.(divide.or.keyprsnt('scale'))
-      call keyr('scale',factor,1.)
-      call keyr('sigma',sigma,0.)
+      selfscal = .not.(divide .or. keyprsnt('scale'))
+      call keyr('scale',factor,1.0)
+      call keyr('sigma',sigma,0.0)
       call keyfin
 c
 c  Check the reasonableness of the inputs.
 c
-      if(map.eq.' ') call bug('f','Input map missing')
-      if(out.eq.' ') call bug('f','Output map missing')
-      if(.not.divide) sigma = 0
-      if(divide.and.sigma.eq.0)
+      if (map.eq.' ') call bug('f','Input map missing')
+      if (out.eq.' ') call bug('f','Output map missing')
+      if (.not.divide) sigma = 0
+      if (divide .and. sigma.eq.0)
      *  call bug('f','Sigma must be set, when using options=divide')
-      if(final.and..not.doGaus)call bug('f',
+      if (final .and. .not.doGaus) call bug('f',
      *  'You cannot set options=final and a beam parameter')
-      if(asym.and.doGaus)then
+      if (asym .and. doGaus) then
         call bug('w','Gaussians are always symmetric')
         asym = .false.
       endif
-      if(.not.asym.and.corr) call bug('w',
+      if (.not.asym .and. corr) call bug('w',
      *'Correlation and convolution do not differ for symmetric beams')
-      if(final.and.divide) call bug('f',
+      if (final .and. divide) call bug('f',
      *'Cannot use options=final and divide together')
 c
 c  Open the map and handle the boxes to be processed.
@@ -175,27 +174,27 @@ c
       nx = nsize(1)
       ny = nsize(2)
       call rdhdi(lMap,'naxis',naxis,MAXNAX)
-      call rdhdd(lMap,'cdelt1',cdelt1,1.d0)
-      call rdhdd(lMap,'cdelt2',cdelt2,1.d0)
+      call rdhdd(lMap,'cdelt1',cdelt1,1d0)
+      call rdhdd(lMap,'cdelt2',cdelt2,1d0)
       naxis = min(naxis,MAXNAX)
-c
+
       call BoxSet(box,3,nsize,' ')
       call BoxInfo(box,3,blc,trc)
-      call BoxMask(lMap,box,maxbox)
+      call BoxMask(lMap,box,MAXBOX)
       rect = BoxRect(box)
 c
 c  Fiddle the gaussian parameters.
 c
       doGaus = beam.eq.' '
-      if(doGaus)then
+      if (doGaus) then
         n1 = nx
         n2 = ny
         iref = nx/2 + 1
         jref = ny/2 + 1
         bmaj1 = (bmaj1/3600.0) * D2R
         bmin1 = (bmin1/3600.0) * D2R
-        if(bmaj1*bmin1.le.0)call bug('f',
-     *    'Either a beam or gaussian must be given')
+        if (bmaj1*bmin1.le.0.0) call bug('f',
+     *    'Either a beam or Gaussian must be given')
 c
 c  Open the beam.
 c
@@ -203,26 +202,26 @@ c
         call xyopen(lBeam,beam,'old',2,nsize)
         n1 = nsize(1)
         n2 = nsize(2)
-        call rdhdr(lBeam,'crpix1',crpix1,real(n1/2+1))
-        call rdhdr(lBeam,'crpix2',crpix2,real(n2/2+1))
+        call rdhdr(lBeam, 'crpix1', crpix1, real(n1/2+1))
+        call rdhdr(lBeam, 'crpix2', crpix2, real(n2/2+1))
         iref = nint(crpix1)
         jref = nint(crpix2)
       endif
 c
 c  Check that the map and beam sizes and deltas are the same.
 c
-      if(nx.gt.n1.or.ny.gt.n2)
+      if (nx.gt.n1 .or. ny.gt.n2)
      *  call bug('f','Map must be smaller than the beam')
 c
 c  If we are operating in "final" mode, determine the convolving
 c  beam.
 c
-      if(final)then
+      if (final) then
         call GauDPar1(lMap,bmaj1,bmin1,bpa1,
      *                                   bmaj,bmin,bpa,temp,ifail)
-        if(ifail.eq.1)call bug('f',
+        if (ifail.eq.1) call bug('f',
      *    'The input has the required final resolution')
-        if(ifail.ne.0)call bug('f',
+        if (ifail.ne.0) call bug('f',
      *    'The convolving beam is undefined for the final resolution')
         bmaj1 = bmaj
         bmin1 = bmin
@@ -230,16 +229,16 @@ c
 
         bmaj = (bmaj*R2D) * 3600.0
         bmin = (bmin*R2D) * 3600.0
-        write (text, 10) bmaj, bmin, bpa
- 10     format ('Convolving Gaussian FWHM:',f5.1,' x',f5.1,
+        write(text, 10) bmaj, bmin, bpa
+ 10     format('Convolving Gaussian FWHM:',f5.1,' x',f5.1,
      *          ' arcsec, PA',f6.1,' deg.')
         call output(text)
       endif
 c
 c  Determine the units,etc, of the output, along with any scale factors.
 c
-      if(selfscal)then
-        if(doGaus)then
+      if (selfscal) then
+        if (doGaus) then
           call GauPar1(lMap,bmaj1,bmin1,bpa1,
      *                           bunit,bmaj,bmin,bpa,factor)
         else
@@ -257,22 +256,21 @@ c  Calculate the transform of the beam.
 c
       l = 0
       flags = ' '
-      if(.not.asym)then
+      if (.not.asym) then
         l = l + 1
         flags(l:l) = 's'
       endif
-      if(divide)then
+      if (divide) then
         l = l + 1
         flags(l:l) = 'd'
       endif
-      if(corr)then
+      if (corr) then
         l = l + 1
         flags(l:l) = 'x'
       endif
       if (doGaus) then
-        call CnvlIniG (handle, n1, n2, iref, jref,
-     *                 bmaj1, bmin1, bpa1, cdelt1, cdelt2,
-     *                 sigma, flags)
+        call CnvlIniG(handle, n1, n2, iref, jref, bmaj1, bmin1, bpa1,
+     *                cdelt1, cdelt2, sigma, flags)
       else
         call CnvlIniF(handle,lBeam,n1,n2,iref,jref,sigma,flags)
         call xyclose(lBeam)
@@ -283,29 +281,29 @@ c
       nsize(1) = trc(1) - blc(1) + 1
       nsize(2) = trc(2) - blc(2) + 1
       nsize(3) = trc(3) - blc(3) + 1
-      do k=4,naxis
+      do k = 4, naxis
         nsize(k) = 1
       enddo
       call xyopen(lOut,Out,'new',naxis,nsize)
       call header(lMap,lOut,min(naxis,3),blc,
      *  bunit,bmaj,bmin,bpa,version)
-      call MemAlloc(pDat,nsize(1)*nsize(2),'r')
+      call MemAllop(pDat,nsize(1)*nsize(2),'r')
 c
 c  Loop over the third dimension.
 c
-      do k=blc(3),trc(3)
+      do k = blc(3), trc(3)
         if (mod(k-blc(3),10).eq.0 .and. blc(3).ne.trc(3))
      *    call output('Beginning plane '//itoaf(k))
 c
 c  Get the run spec. and read in the data.
 c
-        call BoxRuns(1,k,'r',box,Runs,MaxRuns,nRuns,
+        call BoxRuns(1,k,'r',box,Runs,MAXRUNS,nRuns,
      *                                xmin,xmax,ymin,ymax)
         nx = xmax - xmin + 1
         ny = ymax - ymin + 1
         xoff = xmin - blc(1)
         yoff = ymin - blc(2)
-c
+
         call xysetpl(lMap,1,k)
         call GetPlane(lMap,Runs,nRuns,xmin-1,ymin-1,nx,ny,
      *                        memR(pDat),nsize(1)*nsize(2),nPoint)
@@ -316,7 +314,7 @@ c
 c
 c  Apply a scale factor, if needed.
 c
-        if(doscale)call Scale(memR(pDat),nPoint,factor)
+        if (doscale) call Scale(memR(pDat),nPoint,factor)
 c
 c  Write out this plane.
 c
@@ -326,7 +324,7 @@ c
 c
 c  Write out a blanking mask, if needed.
 c
-        if(.not.rect)
+        if (.not.rect)
      *    call PutRuns(lOut,Runs,nRuns,xoff,yoff,nsize(1),nsize(2))
       enddo
 c
@@ -334,13 +332,13 @@ c  All said and done. Close up the files, and leave.
 c
       call xyclose(lOut)
       call xyclose(lMap)
-c
+
       end
 c***********************************************************************
       subroutine GetOpt(final,divide,asym,corr)
-c
+
       logical divide,asym,corr,final
-c
+
 c  Get extra processing options.
 c
 c  Output:
@@ -350,11 +348,11 @@ c    divide     True if we are really deconvolving.
 c    asym       Is the beam asymmetric?
 c-----------------------------------------------------------------------
       integer nopts
-      parameter(nopts=4)
+      parameter (nopts=4)
       logical present(nopts)
       character opts(nopts)*10
       data opts/'divide    ','asymmetric','correlate ','final     '/
-c
+c-----------------------------------------------------------------------
       call options('options',opts,present,nopts)
       divide = present(1)
       asym    = present(2)
@@ -363,10 +361,10 @@ c
       end
 c***********************************************************************
       subroutine Scale(Data,n,factor)
-c
+
       integer n
       real Data(n),factor
-c
+
 c  Multiply by a scale factor.
 c
 c  Input:
@@ -376,24 +374,24 @@ c  In/Out:
 c    Data       The data to scale.
 c-----------------------------------------------------------------------
       integer i
-c
-      do i=1,n
+c-----------------------------------------------------------------------
+      do i = 1, n
         Data(i) = factor * Data(i)
       enddo
       end
 c***********************************************************************
-      subroutine Header(lMap,lOut,naxis,blc,
-     *  bunit,bmaj,bmin,bpa,version)
-c
-      character version*(*),bunit*(*)
-      integer lMap,lOut,naxis,blc(naxis)
-      real bmaj,bmin,bpa
-c
-c  Output the map header.
+      subroutine Header(lMap, lOut, naxis, blc, bunit, bmaj, bmin, bpa,
+     *  version)
+
+      integer   lMap, lOut, naxis, blc(naxis)
+      real      bmaj, bmin, bpa
+      character bunit*(*), version*72
+
+c  Write the output image header.
 c
 c  Inputs:
-c    lMap       Handle of the input map.
-c    lOut       Handle of the output map.
+c    lMap       Handle of the input image.
+c    lOut       Handle of the output image.
 c    naxis      Number of dimensions in the input.
 c    blc        Bottom left corner of the input which is going to
 c               (1,1,1) of the output.
@@ -401,56 +399,38 @@ c    bunit      Units of the output.
 c    bmaj,bmin,bpa Effective beam parameters of the output.
 c    version    Version of this program.
 c-----------------------------------------------------------------------
-      integer i
-      character line*72,num*1
-      real crpix
       integer nkeys
-      parameter(nkeys=37)
-      character keyw(nkeys)*8
-c
-c  Externals.
-c
+      parameter (nkeys=37)
+
+      integer   iax
+      double precision crpix
+      character num*1
+
       character itoaf*2
-c
-      data keyw/
-     *  'cdelt1  ','cdelt2  ','cdelt3  ','cdelt4  ','cdelt5  ',
-     *  'crval1  ','crval2  ','crval3  ','crval4  ','crval5  ',
-     *  'ctype1  ','ctype2  ','ctype3  ','ctype4  ','ctype5  ',
-     *                                   'crpix4  ','crpix5  ',
-     *  'epoch   ','niters  ','object  ','obstime ','cellscal',
-     *  'telescop','history ','restfreq','mostable','pbtype  ',
-     *  'vobs    ','observer','obsra   ','obsdec  ','pbfwhm  ',
-     *  'btype   ','ltype   ','lstart  ','lstep   ','lwidth  '/
-c
-c  Copy keywords across, which have not changed.
-c
-      do i=1,nkeys
-        call hdcopy(lMap,lOut,keyw(i))
+      external  itoaf
+c-----------------------------------------------------------------------
+c     Start by making a verbatim copy of the input image header.
+      call headcp(lMap, lOut, 0, 0, 0, 0)
+
+c     Update the reference pixels.
+      do iax = 1, naxis
+        num = itoaf(iax)
+        call rdhdd(lMap, 'crpix'//num, crpix, 1d0)
+        call wrhdd(lOut, 'crpix'//num, crpix-blc(iax)+1d0)
       enddo
-c
-c  Handle the reference pixels.
-c
-      do i=1,naxis
-        num = itoaf(i)
-        call rdhdr(lMap,'crpix'//num,crpix,1.)
-        call wrhdr(lOut,'crpix'//num,crpix-blc(i)+1)
-      enddo
-c
-c  Set the parameters to determine the units and effective beam.
-c
-      if(bunit.ne.' ')call wrhda(lOut,'bunit',bunit)
-      if(bmaj*bmin.ne.0)then
-        call wrhdr(lOut,'bmaj',bmaj)
-        call wrhdr(lOut,'bmin',bmin)
-        call wrhdr(lOut,'bpa',bpa)
+
+c     Write beam parameters.
+      if (bunit.ne.' ') call wrhda(lOut, 'bunit', bunit)
+      if (bmaj*bmin.ne.0.0) then
+        call wrhdr(lOut, 'bmaj', bmaj)
+        call wrhdr(lOut, 'bmin', bmin)
+        call wrhdr(lOut, 'bpa',  bpa)
       endif
-c
-c  Write the history file.
-c
-      call hisopen(lOut,'append')
-      line = 'CONVOL: Miriad '//version
-      call hiswrite(lOut,line)
-      call hisinput(lOut,'CONVOL')
+
+c     Write history.
+      call hisopen(lOut, 'append')
+      call hiswrite(lOut, 'CONVOL: Miriad '//version)
+      call hisinput(lOut, 'CONVOL')
       call hisclose(lOut)
-c
+
       end
