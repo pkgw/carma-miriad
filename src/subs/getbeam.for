16c8b6b39440fe465caf137809176f7ade35be9b
Marc Pound <mpound@astro.umd.edu>
2015-03-03 22:02:52 +0000

    allow spatial coordinates besides RA/DEC in beam area calculation

--- src/subs/getbeam.for
+++ src/subs/getbeam.for
@@ -23,6 +23,8 @@ c  History:
 c		mchw june 1990
 c    29apr91  mchw  Make cbof exact for Gaussian beam.
 c    30may91  mchw,rjs Check that both RA and DEC axes present.
+c    03mar15  mwp Allow other spatial coordinated beside RA/DEC (was 
+c                 giving wrong answer for (GLAT,GLON)!
 c----------------------------------------------------------------------c
 	double precision pi
 	parameter(pi=3.141592654)
@@ -37,7 +39,9 @@ c
 	do i = 1,naxis
 	  cin = itoaf(i)
 	  call rdhda(lIn,'ctype'//cin,ctype,' ')
-	  if(ctype(1:2).eq.'RA'.or.ctype(1:3).eq.'DEC') then
+c     need to cover ra/dec and glon/glat and elon/elat
+	  if(ctype(1:2).eq.'RA'.or.ctype(1:3).eq.'DEC'
+     * .or.ctype(1:2).eq.'GL'.or.ctype(1:2).eq.'EL') then
 	    call rdhdr(lIn,'cdelt'//cin,cdelt,0.)
 	    omega = omega*cdelt
 	    count = count + 1
